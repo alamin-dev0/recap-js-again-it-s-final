f43993fed05ad8b7fb26932ba4f268f296fb7c5d
Al Amin <alamin.dev01@gmail.com>
2026-04-14 02:29:36 +0600

    added introduction to js code

---
+++ introduction-to-js/math.js
@@ -0,0 +1,17 @@
+/**
+ * ***************************************************
+ * math needed for programming 
+ * ***************************************************
+ * 1. Number : +ve , -ve , 0 [50 ,-9 , 0]
+ * 2.integer : 2, 88, [odd, event], 2, 4, 897
+ * 3. float : 5.6, 2.2
+ * 4. Operation : +, -, *, /
+ * 5. more operation : % , [] 
+ * 6. percentage : 5%, 50 tk er 10% koto?
+ * 7. sum: 14, 18, 16
+ * 8. avg: 14, 18, 16
+ * 9. max / min : 14, 18, 16
+ * 10. series : 
+ * 11. area: Triangle 
+ * 12. 
+ */
---
+++ introduction-to-js/variable.js
@@ -0,0 +1,13 @@
+/**
+ * VARIABLE ==> vary + able 
+ * 5 things to declare a variable 
+ * 
+ */
+
+var weight = 88;
+var price = 38;
+var year = 1999;
+var money = 2003;
+
+var age;
+console.log(money)
