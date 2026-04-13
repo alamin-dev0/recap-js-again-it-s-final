e02c80ff81d5bb4b096bd2f2df53fd1077a414d4
Al Amin <alamin.dev01@gmail.com>
2026-04-14 02:35:12 +0600

    added some code

--- introduction-to-js/variable.js
+++ introduction-to-js/variable.js
@@ -8,6 +8,15 @@ var weight = 88;
 var price = 38;
 var year = 1999;
 var money = 2003;
+money = 500;
 
 var age;
 console.log(money)
+console.log(price)
+
+
+
+
+var age = 17;
+age = 35;
+console.log(age)
