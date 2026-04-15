d3f64d8f32a0313fd828aca79f987bd5fb3bb288
Al Amin <alamin.dev01@gmail.com>
2026-04-16 02:21:56 +0600

    added some code

---
+++ introduction-to-js/nameing.js
@@ -0,0 +1,27 @@
+// 1. no keyword in variable name 
+var false = 45
+var var = 50;
+// 2. no space or gap in variable name 
+var is happy = false 
+var ishapp = true
+
+// 3. no quote 
+var 'address' = 'kocu kha 1';
+
+// can not start start with a number but number other then the fir5st letter is allowed 
+
+
+var money = 2
+var money2= 500
+var 2money = 5000
+
+
+// 5 name is case sensitive 
+address
+Address
+ADDRESS
+// 6. how to write a long variable name 
+ var my current home address = 'Andorkilla bandorban'
+ var my current_home_address = 'Andorkilla bandorban'  // snake case
+ var myCurrentHomeAddress = 'Andorkilla bandorban' // camel case 
+ var MyCurrentHomeAddress = 'Andorkilla bandorban' // pascal case 
--- introduction-to-js/types.js
+++ introduction-to-js/types.js
@@ -1,2 +1,22 @@
-var name = "Sadaruddin";
+// string type variable 
+var name = "Sadar Uddin";
 console.log(name)
+
+var address = "Andor killa bandorban with some katabon"
+
+var country = 'Bangladesh'
+
+//boolean type variable 
+var isRich = true;
+console.log(typeof isRich)
+console.log(isRich)
+
+var passed = false;
+
+console.log(passed)
+console.log(typeof passed)
+console.log(typeof country)
+
+// number type variable
+var price = 555555
+console.log(price)
