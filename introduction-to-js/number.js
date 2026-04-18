beda0d428ff5dfb77b76cf3eb9cd6d856cd9c1fe
Al Amin <alamin.dev01@gmail.com>
2026-04-19 02:49:16 +0600

    push the change

---
+++ introduction-to-js/number.js
@@ -0,0 +1,23 @@
+var orangePrice = 20;
+var chocolate = 0.5
+// var applePrice = parseInt('22.5')
+// var applePrice = parseFloat('22.5')
+var applePrice = '22.5'
+applePrice = parseFloat(applePrice)
+
+
+// console.log(typeof orangePrice)
+// console.log(typeof applePrice)
+// console.log(orangePrice + applePrice)
+
+var first = 0.1
+var second = 0.2;
+var total = first + second
+// console.log(total.toFixed(3))
+const num = -50 / 0
+console.log(num)
+
+const number1 = 5
+const number2 = 2
+const result = number1 % number2
+console.log(result)
