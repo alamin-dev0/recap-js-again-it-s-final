c6eaa8977d843e6da28ef68950417f606822e537
Al Amin <alamin.dev01@gmail.com>
2026-04-24 01:40:57 +0600

    added some code

---
+++ js-conditionals/if.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * tui jodi amake khaoyas tahole ami asbo 
+ * 
+ * 
+ * 
+ * ----------------------
+ * if you buy food fo rme i will come 
+ * 
+ * ---------------------
+ * if biriani price less then 300 i will buy
+ * 
+ * 
+ * --------------------- stucture
+ * if (condition ){
+ * 
+ * }--------------------------
+ * 
+ * if (condition ) {
+ * 
+ * }
+ */
+
+if (13 < 10) {
+    console.log('I have a smaller value ')
+}
+
+
+var biriyaniPrice = 250;
+if (biriyaniPrice < 300) {
+    console.log('mama give me some biriani')
+}
+
+//---------------------------------------------------------
+
+
+var weight = 20
+if (weight <= 20) {
+    console.log('i will carry it myself ')
+}
+else {
+    console.log('i will rent a rickshaw')
+}
