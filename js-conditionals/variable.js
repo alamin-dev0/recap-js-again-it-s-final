155d3a51abb4638a802a142cad73bd93e702ff12
Al Amin <alamin.dev01@gmail.com>
2026-04-24 00:53:55 +0600

    added conditional code

---
+++ js-conditionals/variable.js
@@ -0,0 +1,15 @@
+/**
+ * variable : nothing but a container 
+ * 
+ */
+
+
+// number type variable 
+var sunglassPrice = 90;
+
+
+// string type variable 
+var name = 'alamin'
+
+// Boolean type variable
+var student = true
