b4abc915c814b4784a63a0284ef1e406a2f54187
Al Amin <alamin.dev01@gmail.com>
2026-04-24 02:29:46 +0600

    added some code

---
+++ js-conditionals/multiple.js
@@ -0,0 +1,50 @@
+const salary = 25000;
+const isBCS = true;
+const height = 78;
+const hasCar = false;
+
+// if (salary > 20000 && height > 68) {
+//     console.log('su patro')
+// }
+// else {
+//     console.log('unno patro khuji')
+// }
+
+
+
+
+
+// if(salary > 25000 || height > 72){
+//     console.log('kobul')
+// }
+// else{
+//     console.log('vag tui mokbul')
+// }
+
+
+
+// more and more condition
+// if(salary > 25000 || height > 72 || isBCS == true){
+//     console.log('kobul')
+// }
+// else{
+//     console.log('vag tui mokbul')
+// }
+
+
+
+
+// if(salary > 25000 && height > 72 && isBCS == true){
+//     console.log('kobul')
+// }
+// else{
+//     console.log('vag tui mokbul')
+// }
+
+
+
+
+// ----------------------- complex condition ------------------
+    if ((salary> 25000 || hasCar == true) && isBCS == true){
+        console.log('14 gusti raji ')
+    }
